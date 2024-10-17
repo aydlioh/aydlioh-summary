5a39018f00c858e565e2fb1d864c86c49a750202
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-10-17 23:28:40 +0500

    fix

--- convert.js
+++ convert.js
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 
 const browser = await launch();
 const page = await browser.newPage();
-await page.goto('http://localhost:4321/', {
+await page.goto('http://localhost:4321/editor', {
   waitUntil: 'networkidle2',
 });
 
@@ -12,5 +12,7 @@ const pdf = await page.pdf({
 });
 await browser.close();
 
+await fs.writeFile('./portfolio/public/output.pdf', pdf);
 await fs.writeFile('./output.pdf', pdf);
+
 console.log('Created ./output.pdf')
